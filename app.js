850f7492583644af77c37c47e3dc12eaebc78020
Errafai <errafaiachraf1923@gmail.com>
2024-12-25 11:47:46 +0100

    linking the logging page with mongodb and validate checks

--- app.js
+++ app.js
@@ -6,21 +6,46 @@ const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const cookieParser = require('cookie-parser');
 const connectDB = require("./server/config/db");
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const PORT = 3000 || process.env.PORT;
 
 /**DATABASE */
 connectDB();
 
-app.set('view engine', 'ejs');
+/**cookies and tokens*/
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.use(express.static('public'));
+app.use(cookieParser());
+app.use(session(
+    {
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+          mongoUrl: process.env.MONGODB_URI 
+        })
+    },
+ 
+    //cookie: {maxAge: new Date {Date.now() + (3600000)}}
+));
+
+
 
 /**template */
 app.use(expressEjsLayouts);
 app.set('layout', 'layouts/main');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+
+/**routing */
 app.use("/", require("./server/routes/main"));
 app.use("/", require("./server/routes/users"));
-app.listen(3000, ()=>{
-    console.log("I'am listening on port 3000 ....");
-    
+
+
+app.listen(PORT, ()=>{
+    console.log(`I'am listening on port ${PORT} ....`);
 })
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,15 +4,41 @@ const usersLayout = "../views/layouts/users"
 const User = require('../models/User');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const jwtSecret = process.env.JWT_SECRET;
 /**
  * GET |
  * login page
  */
 router.get('/login', async (req, res) => {
-    res.render("users/login",
-      {layout: usersLayout}
-    );
+    res.render("users/login", {error: null});
+});
+/**
+ * POST
+ * login
+ */
+router.post("/login", async (req, res) =>
+  {
+      
+      try {
+          const {identifier, password} = req.body;
+
+          const user = await User.findOne({$or: [{username: identifier}, {email: identifier}]});
+          if (!user) {
+            return res.render("users/login", {error: "Invalide Cridentials"});
+          }
+          
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+          if (!isPasswordValid) {
+            return res.render("users/login", {error: "Invalide Cridentials"});
+          }
+          
+          const token = jwt.sign({userId: user._id} ,jwtSecret);
+          res.cookie("token", token, {httpOnly: true});
+          res.redirect('/');
+          
+      } catch (error) {
+         console.log(error) 
+      }
 });
 
 /**
@@ -21,6 +47,40 @@ router.get('/login', async (req, res) => {
  */
 router.get('/register', async (req, res) => {
   res.render("users/register");
+});
+
+/**
+ * POST / 
+ * Admin - register
+ */
+
+router.post("/register", async (req, res) =>
+  {
+      
+      try {
+          const {username, password, email, password_again} = req.body;
+          const hashedPassword = await bcrypt.hash(password, 10);
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(email)) {
+            return res.status(400).json({message: "Invalid email"});
+          }
+          if (password !== password_again) {
+            return res.status(400).json({message: "Passwords do not match"});
+          }
+          try {
+           const user = await User.create({username, password:hashedPassword, email});
+           res.status(201).json({
+            message: "User Created", user
+           });
+          } catch (error) {
+            if (error.code === 11000 ) {
+              res.status(409).json({message: "User already in use"});
+            }
+            res.status(500).json({message: "Internal server error"})
+          }
+      } catch (error) {
+         console.log(error) 
+      }
 });
 
 module.exports = router;
